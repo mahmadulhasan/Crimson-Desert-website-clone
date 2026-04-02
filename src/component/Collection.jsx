ef940bbf60a4de6692559251c9a0d55b49959239
MAHMADUL HASAN <mahmadulhasan623@gmail.com>
2026-04-02 15:09:25 +0530

    Update Collection.jsx

--- src/component/Collection.jsx
+++ src/component/Collection.jsx
@@ -8,28 +8,28 @@ const Collection = () => {
             <img src="/public/collectionbg.jpg" alt="" className="absolute w-full h-full z-05" />
             <div className="w-full h-full z-10 absolute bg-gradient-to-b from-blue/60 to-transparent"></div>
 
-            <img src="/public/collection1.png" alt="" className='z-20 h-[50%] w-fit' />
-            <img src="/public/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
+            <img src="/collection1.png" alt="" className='z-20 h-[50%] w-fit' />
+            <img src="/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
             <h2 className="text-3xl  text-white z-20 relative">
                 Standard Edition
             </h2>
         </div>
         <div className="w-full lg:w-[35%] flex flex-col gap-5 items-center justify-center h-[30vh] relative">
-            <img src="/public/collectionbg.jpg" alt="" className="absolute w-full h-full z-05" />
+            <img src="/collectionbg.jpg" alt="" className="absolute w-full h-full z-05" />
             <div className="w-full h-full z-10 absolute bg-gradient-to-b from-[#8d735ea6] to-transparent"></div>
 
-            <img src="/public/collection2.png" alt="" className='z-20 h-[50%] w-fit' />
-            <img src="/public/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
+            <img src="/collection2.png" alt="" className='z-20 h-[50%] w-fit' />
+            <img src="/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
             <h2 className="text-3xl text-white z-20 relative">
                 Deluxe Edition
             </h2>
         </div>
         <div className="w-full lg:w-[40%] flex flex-col gap-5 items-center justify-center h-[30vh] relative">
-            <img src="/public/collectionbg.jpg" alt="" className="absolute w-full h-full z-05" />
+            <img src="/collectionbg.jpg" alt="" className="absolute w-full h-full z-05" />
             <div className="w-full h-full z-10 absolute bg-gradient-to-b from-[#eeb281c0] to-transparent"></div>
 
-            <img src="/public/collection3.png" alt="" className='z-20 h-[50%] w-fit' />
-            <img src="/public/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
+            <img src="/collection3.png" alt="" className='z-20 h-[50%] w-fit' />
+            <img src="/devidor.png" alt="" className="h-[1px] w-[150px] z-20" />
             <h2 className="text-3xl  text-white z-20 relative">
                 Collector's Edition
             </h2>
